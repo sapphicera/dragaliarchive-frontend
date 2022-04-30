a9e6942cd82049ca89eb83e1531e09b54674333d
Alexandra <belliraxia@gmail.com>
2022-04-29 21:13:46 -0400

    fixes user edit/update functionality on profile

--- src/views/UsersEdit.vue
+++ src/views/UsersEdit.vue
@@ -10,8 +10,9 @@ export default {
   },
   created: function () {
     axios.get(`/users/${this.$route.params.username}.json`).then((response) => {
-      console.log(response.data);
       this.editProfileParams = response.data;
+      delete this.editProfileParams['alliance']
+      delete this.editProfileParams['alliance_id']
     })
   },
   methods: {
@@ -41,6 +42,34 @@ export default {
     <div>
       <label>In-Game Name:</label>
       <input type="text" v-model="editProfileParams.ign" />
+    </div>
+    <div>
+      <label>Game ID:</label>
+      <input type="text" v-model="editProfileParams.gameid" />
+    </div>
+    <div>
+      <label>Level:</label>
+      <input type="text" v-model="editProfileParams.level" />
+    </div>
+    <div>
+      <label>Weapon:</label>
+      <input type="text" v-model="editProfileParams.weapon" />
+    </div>
+    <div>
+      <label>Element:</label>
+      <input type="text" v-model="editProfileParams.element" />
+    </div>
+    <div>
+      <label>Event:</label>
+      <input type="text" v-model="editProfileParams.event" />
+    </div>
+    <div>
+      <label>Fight:</label>
+      <input type="text" v-model="editProfileParams.fight" />
+    </div>
+    <div>
+      <label>Description:</label>
+      <input type="text" v-model="editProfileParams.description" />
     </div>
 
     <input type="submit" value="Submit" />
