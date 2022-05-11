74569935b99a37f67f9534ee4c1518270c8446aa
Alexandra <belliraxia@gmail.com>
2022-05-11 18:40:25 -0400

    adds basic comic index page and comics

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,20 +1,74 @@
 <script>
+import axios from "axios";
+import ComicCard from "@/components/ComicCard.vue";
+
 export default {
+  components: { ComicCard },
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      comics: [],
+      // search: "",
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    this.getComicInfo();
+  },
+  methods: {
+    getComicInfo: function () {
+      console.log("getting comic info...");
+      axios.get("/comics.json").then((response) => {
+        console.log(response.data);
+        this.comics = response.data;
+      })
+    }
+    // newSearch: function () {
+    //   console.log("new search...");
+    //   axios.get(`/users.json?search_term=${this.search}`).then(response => {
+    //     console.log(response.data);
+    //     this.users = response.data;
+    //   });
+    // }
+  },
 };
 </script>
 
 <template>
-  <div class="about">
-    <h1>{{ message }}</h1>
+  <!-- Search: <input type="text" v-model="search">
+  <button v-on:click="newSearch()">Search Comics</button>
+
+  <div v-for="user in users" v-bind:key="user.username">
+    {{ user.id }}: {{ user.username }}
+  </div> -->
+
+  <div class="container">
+    <div id="featuredCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="5000">
+          <img src="@/assets/comics/thumb/001.png" class="d-block" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="5000">
+          <img src="@/assets/comics/thumb/002.png" class="d-block" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="5000">
+          <img src="@/assets/comics/thumb/003.png" class="d-block" alt="...">
+        </div>
+      </div>
+    </div>
   </div>
+
+  <ComicCard :comicList="comics" />
 </template>
 
 <style>
+.carousel .carousel-item {
+  height: 24rem;
+  width: 100%;
+}
+
+.carousel-item img {
+  position: absolute;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
 </style>
